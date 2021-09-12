240bf4cb963d6ffffc0ffee01a8778ec4954adb6
sakuow <yusaku1335@gmail.com>
2021-09-12 09:33:42 +0000

    second commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7c80128b1a9155c577cb50117fbaac0c4eeb463efe2a70530a617c2826114ce1eef9891f11498948fc2b189e09c8f4f172cd1c37472a39c6b8b25ec1618e365e'
